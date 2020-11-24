20e55af00068c3c92a3871f7aeb40531378889f3
Philip Martel <pmartel@alum.mit.edu>
2020-11-23 20:59:48 -0500

    Update 28BYJ-48.ino
    This uses logical shift based steppper code.  It's running at a rate where a person can see the phase shifting on the controller board.
    Next step it to make a stepper motor object so we can have two separate ones.

--- 28BYJ-48.ino
+++ 28BYJ-48.ino
@@ -5,11 +5,6 @@
 #define STEPPER_PIN_3 11
 #define STEPPER_PIN_4 12
 
-#define STEPPER2_PIN_1 5
-#define STEPPER2_PIN_2 6
-#define STEPPER2_PIN_3 7
-#define STEPPER2_PIN_4 8
-
 int step_number = 0;
 long delay_u = 1800;
 
@@ -18,119 +13,33 @@ void setup() {
   pinMode(STEPPER_PIN_2, OUTPUT);
   pinMode(STEPPER_PIN_3, OUTPUT);
   pinMode(STEPPER_PIN_4, OUTPUT);
-  pinMode(STEPPER2_PIN_1, OUTPUT);
-  pinMode(STEPPER2_PIN_2, OUTPUT);
-  pinMode(STEPPER2_PIN_3, OUTPUT);
-  pinMode(STEPPER2_PIN_4, OUTPUT);
 }
 
 void loop() {
   int i;
-#ifdef PHIL
-  for( i = 0; i <2048; i++ ){
-    OneStep(false);
-    delayMicroseconds(delay_u);
-  }
-  for( i = 0; i <2048; i++ ){
-    OneStep(true);
-    delayMicroseconds(delay_u);
-  }
-#endif
   OneStep(false);
-  delayMicroseconds(delay_u);
+  //delayMicroseconds(delay_u);
+  delay(100);
 }
 
 
 void OneStep(bool dir){
+  static byte phase = 1;
+  
+  digitalWrite(STEPPER_PIN_1, phase & 1);
+  digitalWrite(STEPPER_PIN_2, phase & 2);
+  digitalWrite(STEPPER_PIN_3, phase & 4);
+  digitalWrite(STEPPER_PIN_4, phase & 8);
   if(dir){
-    switch(step_number){
-    case 0:
-      digitalWrite(STEPPER_PIN_1, HIGH);
-      digitalWrite(STEPPER_PIN_2, LOW);
-      digitalWrite(STEPPER_PIN_3, LOW);
-      digitalWrite(STEPPER_PIN_4, LOW);
-      digitalWrite(STEPPER2_PIN_1, HIGH);
-      digitalWrite(STEPPER2_PIN_2, LOW);
-      digitalWrite(STEPPER2_PIN_3, LOW);
-      digitalWrite(STEPPER2_PIN_4, LOW);
-      break;
-    case 1:
-      digitalWrite(STEPPER_PIN_1, LOW);
-      digitalWrite(STEPPER_PIN_2, HIGH);
-      digitalWrite(STEPPER_PIN_3, LOW);
-      digitalWrite(STEPPER_PIN_4, LOW);
-      digitalWrite(STEPPER2_PIN_1, LOW);
-      digitalWrite(STEPPER2_PIN_2, HIGH);
-      digitalWrite(STEPPER2_PIN_3, LOW);
-      digitalWrite(STEPPER2_PIN_4, LOW);
-      break;
-    case 2:
-      digitalWrite(STEPPER_PIN_1, LOW);
-      digitalWrite(STEPPER_PIN_2, LOW);
-      digitalWrite(STEPPER_PIN_3, HIGH);
-      digitalWrite(STEPPER_PIN_4, LOW);
-      digitalWrite(STEPPER2_PIN_1, LOW);
-      digitalWrite(STEPPER2_PIN_2, LOW);
-      digitalWrite(STEPPER2_PIN_3, HIGH);
-      digitalWrite(STEPPER2_PIN_4, LOW);
-      break;
-    case 3:
-      digitalWrite(STEPPER_PIN_1, LOW);
-      digitalWrite(STEPPER_PIN_2, LOW);
-      digitalWrite(STEPPER_PIN_3, LOW);
-      digitalWrite(STEPPER_PIN_4, HIGH);
-      digitalWrite(STEPPER2_PIN_1, LOW);
-      digitalWrite(STEPPER2_PIN_2, LOW);
-      digitalWrite(STEPPER2_PIN_3, LOW);
-      digitalWrite(STEPPER2_PIN_4, HIGH);
-      break;
-    } 
+    phase <<= 1;
+    if ( phase > 8 ) {
+      phase = 1;
+    }
   }
   else {
-    switch(step_number){
-    case 0:
-      digitalWrite(STEPPER_PIN_1, LOW);
-      digitalWrite(STEPPER_PIN_2, LOW);
-      digitalWrite(STEPPER_PIN_3, LOW);
-      digitalWrite(STEPPER_PIN_4, HIGH);
-      digitalWrite(STEPPER2_PIN_1, LOW);
-      digitalWrite(STEPPER2_PIN_2, LOW);
-      digitalWrite(STEPPER2_PIN_3, LOW);
-      digitalWrite(STEPPER2_PIN_4, HIGH);
-      break;
-    case 1:
-      digitalWrite(STEPPER_PIN_1, LOW);
-      digitalWrite(STEPPER_PIN_2, LOW);
-      digitalWrite(STEPPER_PIN_3, HIGH);
-      digitalWrite(STEPPER_PIN_4, LOW);
-      digitalWrite(STEPPER2_PIN_1, LOW);
-      digitalWrite(STEPPER2_PIN_2, LOW);
-      digitalWrite(STEPPER2_PIN_3, HIGH);
-      digitalWrite(STEPPER2_PIN_4, LOW);
-      break;
-    case 2:
-      digitalWrite(STEPPER_PIN_1, LOW);
-      digitalWrite(STEPPER_PIN_2, HIGH);
-      digitalWrite(STEPPER_PIN_3, LOW);
-      digitalWrite(STEPPER_PIN_4, LOW);
-      digitalWrite(STEPPER2_PIN_1, LOW);
-      digitalWrite(STEPPER2_PIN_2, HIGH);
-      digitalWrite(STEPPER2_PIN_3, LOW);
-      digitalWrite(STEPPER2_PIN_4, LOW);
-      break;
-    case 3:
-      digitalWrite(STEPPER_PIN_1, HIGH);
-      digitalWrite(STEPPER_PIN_2, LOW);
-      digitalWrite(STEPPER_PIN_3, LOW);
-      digitalWrite(STEPPER_PIN_4, LOW);
-      digitalWrite(STEPPER2_PIN_1, HIGH);
-      digitalWrite(STEPPER2_PIN_2, LOW);
-      digitalWrite(STEPPER2_PIN_3, LOW);
-      digitalWrite(STEPPER2_PIN_4, LOW);
-    } 
-  }
-  step_number++;
-  if(step_number > 3){
-    step_number = 0;
+    phase >>= 1;
+    if ( phase < 1 ) {
+      phase = 8;
+    }
   }
 }
